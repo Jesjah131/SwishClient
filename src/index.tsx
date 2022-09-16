2525b187ee5374fe470c05b832ea19edac45186b
jesper j <jesperjansson@live.se>
2022-09-16 13:18:56 +0200

    -

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { MainComponent } from "./App";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(<MainComponent />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<MainComponent />, rootElement);
+} else {
+  render(<MainComponent />, rootElement);
+}
